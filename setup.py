a88620a607f37272a946e1bc32dc22be47532a5d
Daniel Høyer Iversen <mail@dahoiv.net>
2018-09-29 08:57:27 +0200

    0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PyMetno',
     packages = ['metno'],
     install_requires=['xmltodict', 'aiohttp', 'async_timeout', 'pytz'],
-    version = '0.2.0',
+    version = '0.3.0',
     description = 'A library to communicate with the met.no api',
     author='Daniel Hoyer Iversen',
     url='https://github.com/Danielhiversen/pyMetno/',
