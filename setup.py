c20ee13df5810375ebef4669c2a2ec2023a4620b
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-04-08 20:36:01 +0200

    0.8.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PyMetno',
     packages = ['metno'],
     install_requires=['xmltodict', 'aiohttp', 'async_timeout', 'pytz'],
-    version = '0.8.1',
+    version = '0.8.2',
     description = 'A library to communicate with the met.no api',
     author='Daniel Hjelseth Høyer',
     url='https://github.com/Danielhiversen/pyMetno/',
